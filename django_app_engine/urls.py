4c76de36bc18f6f2d5b4871697cc664941d54833
Richard Lewis <rlewis@decisionvis.com>
2016-12-16 11:09:54 -0500

    modified urls to match

--- django_app_engine/urls.py
+++ django_app_engine/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls import include
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
+    url(r'^', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+# This enables static files to be served from the Gunicorn server
+# In Production, serve static files from Google Cloud Storage or an alternative
+# CDN
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
